651d8c314a45a96881e5c8b5b82ee20d3490d2a6
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-20 11:29:30 +0300

    According to ERD Added :author to CreatePost migrate instead of :user

--- db/migrate/20230919194135_create_posts.rb
+++ db/migrate/20230919194135_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.belongs_to :user, foreign_key: { to_table: :users }, index: true
+      t.belongs_to :author, foreign_key: { to_table: :users }, index: true
       t.string :title
       t.string :text
       t.integer :comments_counter
