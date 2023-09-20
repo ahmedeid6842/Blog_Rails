d1ae468ac21e8923985aef12c9d46da0262f9f62
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-20 20:51:48 +0300

    Implememnt update post likes counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  after_save :update_post_likes_counter
+  after_destroy :update_post_likes_counter
+
+  def update_post_likes_counter
+    post.update(liked_counter: post.likes.count)
+  end
 end
