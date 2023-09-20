48f6384ba843978e4acb5d1288f10b2421ce85ee
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-20 20:46:22 +0300

    Implement custom get post's recent comment method

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,8 @@ class Post < ApplicationRecord
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
   end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
