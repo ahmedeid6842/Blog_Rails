10a6323d8615d0e67e8a21b188f740e5b79f6c7a
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-20 20:53:52 +0300

    Added likes realtion at post table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :author_id
   has_many :comments
+  has_many :likes
 
   after_create :update_user_posts_counter
 
