fe36306516457e06788a99dcd254414bb293e5c8
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-20 20:43:11 +0300

    Implememnt custom update user posts counter hooks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :author_id
   has_many :comments
+
+  after_create :update_user_posts_counter
+
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
