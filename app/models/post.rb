6d869808b1c0dbf7a904339c47de61de47370222
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-21 22:35:34 +0300

    Change the after_create hook to after_save at post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,15 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  after_create :update_user_posts_counter
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_user_posts_counter
+
+  private
 
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
-  end
-
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
   end
 end
