2982f508b384255a8aa5e27444aec9a14169b235
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-21 23:27:27 +0300

    Setup rails spec test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# rubocop:disable Metrics/BlockLength
-ActiveRecord::Schema[7.0].define(version: 20_230_920_172_030) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_172030) do
   # These are extensions that must be enabled in order to support this database
-  # rubocop:enable Metrics/BlockLength
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
