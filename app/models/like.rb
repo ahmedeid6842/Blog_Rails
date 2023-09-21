f2ac5ee5500e38861347db11236306141cac21e5
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-21 22:38:28 +0300

    change the update post like counter to be private

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,6 +5,8 @@ class Like < ApplicationRecord
   after_save :update_post_likes_counter
   after_destroy :update_post_likes_counter
 
+  private
+
   def update_post_likes_counter
     post.update(liked_counter: post.likes.count)
   end
