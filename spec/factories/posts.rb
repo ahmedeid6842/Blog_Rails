5c510afbb6eebadf1e92238b46b47d25fcbf8d68
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-28 02:47:42 +0300

    Implement the post factorybot

---
+++ spec/factories/posts.rb
@@ -0,0 +1,12 @@
+# spec/factories/posts.rb
+FactoryBot.define do
+  factory :post do
+    # Define the attributes for the Post model
+    # Example:
+    title { "Sample Title" }
+    liked_counter { 5 }
+    comments_counter { 1 }
+    # You can associate the post with the user by defining the author association
+    association :author, factory: :user
+  end
+end
