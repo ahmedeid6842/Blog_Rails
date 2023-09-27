b337cf0ee5fce108b8433bd8f43352b133fbe488
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-28 02:49:05 +0300

    Implement posts controller test Get#show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -23,4 +23,25 @@ RSpec.describe PostsController, type: :request do
       expect(response.body).to include("List of Posts")
     end
   end
+
+  describe "GET #show" do
+    user = FactoryBot.create(:user, name: "ahmed", photo: "https://unsplash.com/photos/F_-0BxGuVvo",
+                                    bio: "A genius Backend developer from Egypt.", posts_counter: 0)
+    post = FactoryBot.create(:post, author: user, title: "Sample Title", liked_counter: 5, comments_counter: 1)
+
+    it "returns a success response" do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it "includes correct placeholder text in the response body" do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to include("Post Details")
+    end
+  end
 end
