2c6a87d29d47413cd617e2ab517ecccdbadf4065
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-29 05:29:53 +0300

    Fixed the bug of miss redirect after adding comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = params[:post_id]
     if @comment.save
-      redirect_to user_post_path(current_user.id, params[:post_id])
+      redirect_to user_post_path(params[:user_id], params[:post_id])
     else
       render :create
     end
