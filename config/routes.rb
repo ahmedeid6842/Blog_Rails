6af74717c0cf75eb6e232a7d6f372ca5cd7d9b33
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-30 00:45:34 +0300

    Implement the required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  resources :users do
-    resources :posts do
-      resources :comments
-      resources :likes
-    end
-  end
+  get "/users", to: "users#index"
 
-  resources :posts
+  get "/users/:id", to: "users#show", as: "user"
+
+  get "/users/:user_id/posts/new", to: "posts#new", as: "new_user_post"
+  post "/users/:user_id/posts", to: "posts#create", as: "user_posts_create"
+
+  get "/users/:user_id/posts/:post_id/comments/new", to: "comments#new", as: "new_user_post_comment"
+  post "/users/:user_id/posts/:post_id/comments", to: "comments#create", as: "user_post_comments"
+
+  post "/users/:user_id/posts/:post_id/likes", to: "likes#create", as: "user_post_likes"
+
+  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
+
+  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
 end
