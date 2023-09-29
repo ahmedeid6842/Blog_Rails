e4b2e71816c26f7508e020aa787b9fab65d2c03d
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-30 01:31:46 +0300

    Implement create new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
   def create
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = params[:post_id]
     if @comment.save
       redirect_to user_post_path(params[:user_id], params[:post_id])
     else
-      render :create
+      render :new
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get "/users/:user_id/posts/new", to: "posts#new", as: "new_user_post"
   post "/users/:user_id/posts", to: "posts#create", as: "user_posts_create"
+  post "/users/:user_id/posts", to: "posts#create", as: "posts"
 
   get "/users/:user_id/posts/:post_id/comments/new", to: "comments#new", as: "new_user_post_comment"
   post "/users/:user_id/posts/:post_id/comments", to: "comments#create", as: "user_post_comments"
