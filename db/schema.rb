03eb63b27d00478cfbfbb015155c41c4e51c1f91
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-29 05:39:54 +0300

    Fixed rubocop linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -9,9 +9,11 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+# rubocop:disable Metrics/BlockLength
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_29_020947) do
+ActiveRecord::Schema[7.0].define(version: 20_230_929_020_947) do
   # These are extensions that must be enabled in order to support this database
+  # rubocop:enable Metrics/BlockLength
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
