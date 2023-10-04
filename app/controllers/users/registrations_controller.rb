859904d318f0c0d2b30b22d13cd5510fb112b263
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-05 01:41:50 +0300

    setup the signup parameters for registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,4 +59,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+
+  def sign_up_params
+    puts "#{:name}"
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
 end
