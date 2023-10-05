4995ec823ca39089ae21e026f95f0b2776242d67
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-05 20:39:48 +0300

    Add destroy comment ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,10 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.role == "admin"
       can :destroy, Post
+      can :destroy, Comment
     else
       can :destroy, Post, author_id: user.id
+      can :destroy, Comment, author_id: user.id
     end
   end
 end
